857083ae86c8d2bd67d600b07d76ddc4f7c2a799
bonsoleil <33630190+bonsoleil@users.noreply.github.com>
2018-02-20 15:50:28 +0100

    Update index.php

--- test/index.php
+++ test/index.php
@@ -39,7 +39,7 @@
 
         $conexion = mysqli_connect( $servidor, $usuario, "" ) or die ("No se ha podido conectar al servidor de Base de datos");
 	$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
-
+	
 
 	mysqli_close( $conexion );
 	?>

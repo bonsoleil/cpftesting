faa8734f97e9dd94d72aa1b9db8842d8b1816092
bonsoleil <33630190+bonsoleil@users.noreply.github.com>
2018-04-08 13:47:05 +0200

    Update index.php

--- test/index.php
+++ test/index.php
@@ -1,47 +1,44 @@
 <!DOCTYPE html>
 <head>
-<title>TEST</title>
+	<title> CPF - 6ÈME</title>
 </head>
 <body>
 <center>
-	<h1>TEST 1</h1>
+	<h1>6ème CPF v1</h1>
 	<br>
-	<h1>Temp:</h1>
-	<span id=tempValue>none</span>
+	<h1>Temperature Sensor</h1>
+	<span id="tempvalue">none</span>
 </center>
+
 <script>
-	
-	var temp;
-	
-	function main() {
-		if(cpf){
-			temp = cpf.get("a0");
-			document.getElementById("tempValue").innerHTML = temp;
-			
+
+var temp;
+var tempconverted;
+
+	function toCelsius(value) {
+		var resistance = parseFloat((1023-value) * 10000 / value);
+		var temperature = 1 / (Math.log(resistance / 10000) / 3975+1 / 298.15) - 273.15;
+
+		return temperature.toFixed(2);
+	}
+
+	function main(){
+		if (cpf){
+			temp = cpf.get("temperature sensor");
+			tempconverted = toCelsius(temp);
+			document.getElementById("tempvalue").innerHTML = tempconverted;
 		}
 		setTimeout(main, 1000);
 	}
-	
-	function setup() {
-		cpf.setPinMode('["resetPin"],["setPinMode", "analog", 0,"INPUT"]');
-		
+
+	function setup(){
+		if (cpf) {
+			cpf.setPinMode('["resetPin"],["setPinMode", "analog", 0, "INPUT"]');
+		}
 	}
-	
-	setup();
-	main();
-	
-</script>
-	<?php
-	$usuario = "root";
-	$password = "";
-	$servidor = "localhost";
-	$basededatos = "CPF";
 
-        $conexion = mysqli_connect( $servidor, $usuario, "" ) or die ("No se ha podido conectar al servidor de Base de datos");
-	$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
-	
-
-	mysqli_close( $conexion );
-	?>
+		setup();
+		main();
+	</script>
 </body>
 </html>
